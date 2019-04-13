927b1b9ef894d0a9e91e4df0eace38569f45e243
ado <adolsen@gmail.com>
2019-04-13 14:30:51 +0200

    colors

---
+++ Assets/Scipts/ChangeColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+
+	private List<Color> colors = new List<Color> { Color.red, Color.blue, Color.yellow, Color.green, Color.magenta, Color.cyan, Color.magenta };
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    this.GetComponent<Renderer>().material.color = colors[Random.Range(0, colors.Count)];
+    }
+}
