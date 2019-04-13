0d72c77913bd2c115082e1f9f2e5151ecf88761d
sm1rt19 <sm1rt19@lortemail.com>
2019-04-13 20:14:36 +0200

    Ready button working. Can check/uncheck ready state.

--- Assets/Carsten/LevelMenuController.cs
+++ Assets/Carsten/LevelMenuController.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class LevelMenuController : MonoBehaviour
 {
-    //bool 
+    private bool[] playerReady = new bool[4];
+    private int playersReady;
+    private bool gameStart;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < playerReady.Length; i++)
+            playerReady[i] = false;
+        playersReady = 0;
+        gameStart = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameReady();
     }
 
-    public void ReadyClick(int i)
+    public void ReadyClick(int playerNumber)
     {
-        //print("Player" + i + "ready");
-        var str = string.Format("Player {0} ready", i);
-        print(str);
+        playersReady = playerReady[playerNumber - 1] ? playersReady - 1 : playersReady + 1;
+        playerReady[playerNumber - 1] = !playerReady[playerNumber - 1];
+        for (int i = 0; i < playerReady.Length; i++)
+            print(string.Format("Player {0} ready: {1} | Game start: {2}", i, playerReady[i], gameStart));
+    }
+
+    public void GameReady()
+    {
+        gameStart = playersReady == playerReady.Length;
     }
 }
