6172a650aa81f3e9dca1718f7c5765333e5624e2
Lasse Andresen <la@dalux.com>
2019-04-13 17:23:54 +0200

    Fixed player script

--- Assets/Scipts/Player.cs
+++ Assets/Scipts/Player.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 	public CharacterController Controller;
-	public float Speed = 2;
-	public int Health = 5;
+	public SpriteRenderer HealthBar;
+	public SpriteRenderer ShieldBar;
+	public float Speed;
+	public float Health;
+	public float Shield;
 
     // Start is called before the first frame update
     void Start()
     {
 	    Controller = this.GetComponent<CharacterController>();
 	    Controller.enabled = true;
+		UpdateHealthBar();
+		UpdateShieldBar();
 	}
 
 	// Update is called once per frame
@@ -21,16 +26,50 @@ public class Player : MonoBehaviour
 	    var dx = Input.GetAxis("Horizontal");
 	    var dy = Input.GetAxis("Vertical");
 		Controller.SimpleMove(new Vector3(dx, 0, dy) * Speed);
-    }
+	}
 
-	public void TakeDamage(int damage)
+	public void TakeDamage(float damage)
 	{
-		Health -= damage;
+		var healthDamage = 0f;
+		
+		if (Shield > 0f)
+		{
+			float shieldLost;
+			if (Shield > damage)
+			{
+				shieldLost = damage;
+				Shield -= damage;
+				UpdateShieldBar();
+			}
+			else
+			{
+				shieldLost = Shield;
+				healthDamage = damage - shieldLost;
+			}
+			UpdateShieldBar();
+		}
+		else
+		{
+			healthDamage = damage;
+		}
 
-		if (Health <= 0)
+		Health -= healthDamage;
+		UpdateHealthBar();
+
+		if (Health <= 0f)
 		{
 			print("Seppuku!!!");
 			this.gameObject.SetActive(false);
 		}
+	}
+
+	public void UpdateHealthBar()
+	{
+		HealthBar.transform.localScale = new Vector3(Health / 10.53f, 1.92f, 1.23f);
+	}
+
+	public void UpdateShieldBar()
+	{
+		ShieldBar.transform.localScale = new Vector3(Shield / 10.53f, 1.92f, 1.23f);
 	}
 }
