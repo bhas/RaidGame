58aea3f75a0a27c83cd987916bdfae05462cc2b6
Lasse Andresen <la@dalux.com>
2019-04-13 18:04:10 +0200

    Fixed healthbars and merged player script

--- Assets/Scipts/Player.cs
+++ Assets/Scipts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,14 +11,19 @@ public class Player : MonoBehaviour
 	public KeyCode down;
 
 	public CharacterController Controller;
+	public SpriteRenderer HealthBar;
+	public SpriteRenderer ShieldBar;
 	public float Speed = 2;
-	public int Health = 5;
+	public float Health;
+	public float Shield;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-	    Controller = this.GetComponent<CharacterController>();
-	    Controller.enabled = true;
+	// Start is called before the first frame update
+	void Start()
+	{
+		Controller = this.GetComponent<CharacterController>();
+		Controller.enabled = true;
+		UpdateHealthBar();
+		UpdateShieldBar();
 	}
 
 	// Update is called once per frame
@@ -45,18 +51,67 @@ public class Player : MonoBehaviour
 			dz = -1;
 		}
 		Controller.SimpleMove(new Vector3(dx, 0, dz) * Speed);
-
-
 	}
 
-	public void TakeDamage(int damage)
+	public void TakeDamage(float damage)
 	{
-		Health -= damage;
+		var healthDamage = 0f;
 
-		if (Health <= 0)
+		if (Shield > 0f)
+		{
+			float shieldLost;
+			if (Shield > damage)
+			{
+				shieldLost = damage;
+				Shield -= damage;
+				UpdateShieldBar();
+			}
+			else
+			{
+				shieldLost = Shield;
+				healthDamage = damage - shieldLost;
+			}
+			UpdateShieldBar();
+		}
+		else
+		{
+			healthDamage = damage;
+		}
+
+		Health -= healthDamage;
+		UpdateHealthBar();
+
+		if (Health <= 0f)
 		{
 			print("Seppuku!!!");
 			this.gameObject.SetActive(false);
+		}
+	}
+
+	public void UpdateHealthBar()
+	{
+		HealthBar.transform.localScale = new Vector3(Health / 10.53f, 1.92f, 1.23f);
+		if (Health == 100)
+		{
+			HealthBar.gameObject.SetActive(false);
+		}
+		else if (!HealthBar.gameObject.activeSelf)
+		{
+			HealthBar.gameObject.SetActive(true);
+		}
+
+	}
+
+	public void UpdateShieldBar()
+	{
+		ShieldBar.transform.localScale = new Vector3(Shield / 10.53f, 1.92f, 1.23f);
+		if (Shield == 100 || Shield == 0)
+		{
+			ShieldBar.gameObject.SetActive(false);
+		}
+		else if (!HealthBar.gameObject.activeSelf)
+		{
+			ShieldBar.gameObject.SetActive(true);
 		}
 	}
 }
