a2dfa65fe78bb70edbf46f8a4b05467c5680e760
ado <adolsen@gmail.com>
2019-04-13 17:53:19 +0200

    multiplayer

--- Assets/Scipts/Player.cs
+++ Assets/Scipts/Player.cs
@@ -4,72 +4,59 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	public KeyCode left;
+	public KeyCode right;
+	public KeyCode up;
+	public KeyCode down;
+
 	public CharacterController Controller;
-	public SpriteRenderer HealthBar;
-	public SpriteRenderer ShieldBar;
-	public float Speed;
-	public float Health;
-	public float Shield;
+	public float Speed = 2;
+	public int Health = 5;
 
     // Start is called before the first frame update
     void Start()
     {
 	    Controller = this.GetComponent<CharacterController>();
 	    Controller.enabled = true;
-		UpdateHealthBar();
-		UpdateShieldBar();
 	}
 
 	// Update is called once per frame
 	void Update()
-    {
-	    var dx = Input.GetAxis("Horizontal");
-	    var dy = Input.GetAxis("Vertical");
-		Controller.SimpleMove(new Vector3(dx, 0, dy) * Speed);
+	{
+		var dx = 0;
+		var dz = 0;
+		if (Input.GetKey(left))
+		{
+			dx = -1;
+		}
+
+		if (Input.GetKey(right))
+		{
+			dx = 1;
+		}
+
+		if (Input.GetKey(up))
+		{
+			dz = 1;
+		}
+
+		if (Input.GetKey(down))
+		{
+			dz = -1;
+		}
+		Controller.SimpleMove(new Vector3(dx, 0, dz) * Speed);
+
+
 	}
 
-	public void TakeDamage(float damage)
+	public void TakeDamage(int damage)
 	{
-		var healthDamage = 0f;
-		
-		if (Shield > 0f)
-		{
-			float shieldLost;
-			if (Shield > damage)
-			{
-				shieldLost = damage;
-				Shield -= damage;
-				UpdateShieldBar();
-			}
-			else
-			{
-				shieldLost = Shield;
-				healthDamage = damage - shieldLost;
-			}
-			UpdateShieldBar();
-		}
-		else
-		{
-			healthDamage = damage;
-		}
+		Health -= damage;
 
-		Health -= healthDamage;
-		UpdateHealthBar();
-
-		if (Health <= 0f)
+		if (Health <= 0)
 		{
 			print("Seppuku!!!");
 			this.gameObject.SetActive(false);
 		}
-	}
-
-	public void UpdateHealthBar()
-	{
-		HealthBar.transform.localScale = new Vector3(Health / 10.53f, 1.92f, 1.23f);
-	}
-
-	public void UpdateShieldBar()
-	{
-		ShieldBar.transform.localScale = new Vector3(Shield / 10.53f, 1.92f, 1.23f);
 	}
 }
