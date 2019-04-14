8e388b1fbd2d915c18b5883895569ce9ddf9c382
Bjørn Vinther <bv@dalux.com>
2019-04-14 18:06:23 +0200

    Added inputs for ps4 controllers

---
+++ Assets/Bjørn/InputTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTest : MonoBehaviour
+{
+    public int player;
+    private Ps4Input input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = new Ps4Input(player);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var speed = .5f;
+        var movement = speed * input.GetLeftAnalog();
+        transform.Translate(movement.x, 0, movement.y);
+
+        if (input.GetButtonDown(Ps4Button.Cross))
+            print($"Player {player} pressed {Ps4Button.Cross}");
+        if (input.GetButtonDown(Ps4Button.Square))
+            print($"Player {player} pressed {Ps4Button.Square}");
+        if (input.GetButtonDown(Ps4Button.Circle))
+            print($"Player {player} pressed {Ps4Button.Circle}");
+        if (input.GetButtonDown(Ps4Button.Triangle))
+            print($"Player {player} pressed {Ps4Button.Triangle}");
+    }
+}
---
+++ Assets/Bjørn/Ps4Input.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ps4Input
+{
+    public int player;
+    public Ps4Input(int player)
+    {
+        this.player = player;
+    }
+
+    public Vector2 GetLeftAnalog()
+    {
+        var dx = Input.GetAxis($"P{player}_Left_Horizontal");
+        var dy = Input.GetAxis($"P{player}_Left_Vertical");
+        return new Vector2(dx, dy);
+    }
+
+    public Vector2 GetRightAnalog()
+    {
+        var dx = Input.GetAxis($"P{player}_Right_Horizontal");
+        var dy = Input.GetAxis($"P{player}_Right_Vertical");
+        return new Vector2(dx, dy);
+    }
+
+    public bool GetButtonDown(Ps4Button button)
+    {
+        var keyCode = GetKeyCode(button);
+        return Input.GetKeyDown(keyCode);
+    }
+
+    public bool GetButtonUp(Ps4Button button)
+    {
+        var keyCode = GetKeyCode(button);
+        return Input.GetKeyUp(keyCode);
+    }
+
+    public bool GetButton(Ps4Button button)
+    {
+        var keyCode = GetKeyCode(button);
+        Console.WriteLine("Keycode");
+        return Input.GetKey(keyCode);
+    }
+
+    private KeyCode GetKeyCode(Ps4Button button)
+    {
+        switch (button)
+        {
+            case Ps4Button.Square:
+                if (player == 0) return KeyCode.J;
+                if (player == 1) return KeyCode.Joystick1Button0;
+                if (player == 2) return KeyCode.Joystick2Button0;
+                if (player == 3) return KeyCode.Joystick3Button0;
+                if (player == 4) return KeyCode.Joystick4Button0;
+                break;
+            case Ps4Button.Cross:
+                if (player == 0) return KeyCode.K;
+                if (player == 1) return KeyCode.Joystick1Button1;
+                if (player == 2) return KeyCode.Joystick2Button1;
+                if (player == 3) return KeyCode.Joystick3Button1;
+                if (player == 4) return KeyCode.Joystick4Button1;
+                break;
+            case Ps4Button.Circle:
+                if (player == 0) return KeyCode.L;
+                if (player == 1) return KeyCode.Joystick1Button2;
+                if (player == 2) return KeyCode.Joystick2Button2;
+                if (player == 3) return KeyCode.Joystick3Button2;
+                if (player == 4) return KeyCode.Joystick4Button2;
+                break;
+            case Ps4Button.Triangle:
+                if (player == 0) return KeyCode.I;
+                if (player == 1) return KeyCode.Joystick1Button3;
+                if (player == 2) return KeyCode.Joystick2Button3;
+                if (player == 3) return KeyCode.Joystick3Button3;
+                if (player == 4) return KeyCode.Joystick4Button3;
+                break;
+        }
+
+        throw new NotImplementedException();
+    }
+}
+
+public enum Ps4Button
+{
+    Cross, Square, Circle, Triangle
+}
